4b4b90dfcacbce53ee4e94243e8d416b6a9b71d8
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-12 21:25:04 -0700

    added benchmarks folder

--- c/benchmark.sh
+++ c/benchmark.sh
@@ -4,6 +4,6 @@ for i in 6 7 8
 do
 python ../specializers/z_generator.py 3 $i > ../c/zjacobi.c
 gcc -std=c99 -O3 -fopenmp -w $2.c -o $2
-echo $i >> $2$1.log
+echo $i >> benchmarks/$2$1.log
 ./zjacobi_main $i $1 >> benchmarks/$2$1.log
 done
