3391e48a1218f68146b88a2ae3d93f4e475a9c8f
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-12 21:13:45 -0700

    added benchmark

---
+++ c/benchmark.sh
@@ -0,0 +1,10 @@
+@/reserve/reserve.me
+source activate ucb-sejits
+for i in 6 7 8
+do
+python ../specializers/z_generator.py 3 $i > ../c/zjacobi.c
+gcc -std=c99 -O3 -fopenmp -w zjacobi_main.c -o zjacobi_main_3_$i
+echo $i >> z$1.log
+./zjacobi_main $i $1 >> z$1.log
+done
+@/reserve/unreserve.me
