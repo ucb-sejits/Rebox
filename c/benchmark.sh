e6c129a0630edf5a233687ef9a7c5f7b990a7fa6
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-12 21:17:43 -0700

    fixing benchmark

--- c/benchmark.sh
+++ c/benchmark.sh
@@ -1,11 +1,9 @@
-@ /reserve/reserve.me
 source activate ucb-sejits
 touch z$1.log
 for i in 6 7 8
 do
 python ../specializers/z_generator.py 3 $i > ../c/zjacobi.c
-gcc -std=c99 -O3 -fopenmp -w zjacobi_main.c -o zjacobi_main_3_$i
+gcc -std=c99 -O3 -fopenmp -w zjacobi_main.c -o zjacobi_main
 echo $i >> z$1.log
-./zjacobi_main $i $1 >> z$1.log
+./zjacobi_main $i $1
 done
-@ /reserve/unreserve.me
