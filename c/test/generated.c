7489343bbdb784de193008ae5f9ffdcf802de572
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-18 23:31:37 -0700

    modified main

--- c/test/generated.c
+++ c/test/generated.c
@@ -12,85 +12,85 @@ void apply(double* arr, double* out) {
         
         size_t delta;
         
-        total += arr[clamp(add(index, 18446744073709551615lu))] * 1l;
+        total += arr[clamp(add(index, 18446744073709551615lu))];
 
         
-        total += arr[clamp(add(index, 13176245766935394011lu))] * 1l;
+        total += arr[clamp(add(index, 13176245766935394011lu))];
 
         
-        total += arr[clamp(add(index, 13176245766935394015lu))] * 1l;
+        total += arr[clamp(add(index, 13176245766935394015lu))];
 
         
-        total += arr[clamp(add(index, 15811494920322472813lu))] * 1l;
+        total += arr[clamp(add(index, 15811494920322472813lu))];
 
         
-        total += arr[clamp(add(index, 10540996613548315209lu))] * 1l;
+        total += arr[clamp(add(index, 10540996613548315209lu))];
 
         
-        total += arr[clamp(add(index, 10540996613548315213lu))] * 1l;
+        total += arr[clamp(add(index, 10540996613548315213lu))];
 
         
-        total += arr[clamp(add(index, 15811494920322472815lu))] * 1l;
+        total += arr[clamp(add(index, 15811494920322472815lu))];
 
         
-        total += arr[clamp(add(index, 10540996613548315211lu))] * 1l;
+        total += arr[clamp(add(index, 10540996613548315211lu))];
 
         
-        total += arr[clamp(add(index, 10540996613548315215lu))] * 1l;
+        total += arr[clamp(add(index, 10540996613548315215lu))];
 
         
-        total += arr[clamp(add(index, 7905747460161236406lu))] * 1l;
+        total += arr[clamp(add(index, 7905747460161236406lu))];
 
         
-        total += arr[clamp(add(index, 2635249153387078802lu))] * 1l;
+        total += arr[clamp(add(index, 2635249153387078802lu))];
 
         
-        total += arr[clamp(add(index, 2635249153387078806lu))] * 1l;
+        total += arr[clamp(add(index, 2635249153387078806lu))];
 
         
-        total += arr[clamp(add(index, 5270498306774157604lu))] * 1l;
+        total += arr[clamp(add(index, 5270498306774157604lu))];
 
         
-        total += arr[clamp(add(index, 0lu))] * 1l;
+        total += arr[index];
 
         
-        total += arr[clamp(add(index, 4lu))] * 1l;
+        total += arr[clamp(add(index, 4lu))];
 
         
-        total += arr[clamp(add(index, 5270498306774157606lu))] * 1l;
+        total += arr[clamp(add(index, 5270498306774157606lu))];
 
         
-        total += arr[clamp(add(index, 2lu))] * 1l;
+        total += arr[clamp(add(index, 2lu))];
 
         
-        total += arr[clamp(add(index, 6lu))] * 1l;
+        total += arr[clamp(add(index, 6lu))];
 
         
-        total += arr[clamp(add(index, 7905747460161236407lu))] * 1l;
+        total += arr[clamp(add(index, 7905747460161236407lu))];
 
         
-        total += arr[clamp(add(index, 2635249153387078803lu))] * 1l;
+        total += arr[clamp(add(index, 2635249153387078803lu))];
 
         
-        total += arr[clamp(add(index, 2635249153387078807lu))] * 1l;
+        total += arr[clamp(add(index, 2635249153387078807lu))];
 
         
-        total += arr[clamp(add(index, 5270498306774157605lu))] * 1l;
+        total += arr[clamp(add(index, 5270498306774157605lu))];
 
         
-        total += arr[clamp(add(index, 1lu))] * 1l;
+        total += arr[clamp(add(index, 1lu))];
 
         
-        total += arr[clamp(add(index, 5lu))] * 1l;
+        total += arr[clamp(add(index, 5lu))];
 
         
-        total += arr[clamp(add(index, 5270498306774157607lu))] * 1l;
+        total += arr[clamp(add(index, 5270498306774157607lu))];
 
         
-        total += arr[clamp(add(index, 3lu))] * 1l;
+        total += arr[clamp(add(index, 3lu))];
 
         
-        total += arr[clamp(add(index, 7lu))] * 1l;
+        total += arr[clamp(add(index, 7lu))];
 
 
         
--- c/test/main.c
+++ c/test/main.c
@@ -6,9 +6,13 @@ int main (int argc, const char* argv[])
 	double* in = (double*) malloc(sizeof(double) * 1024*1024*1024);
 	double* out = (double*) malloc(sizeof(double) * 1024 * 1024 * 1024);
 	double t = omp_get_wtime();
-	apply(in, out);
+	for (int i = 0; i < 5; i++)
+	{
+		apply(in, out);
+	}
 	double t2 = omp_get_wtime();
 	free(in);
 	free(out);
 	printf("Time: %f\n", t2-t);
+	return 0;
 }
