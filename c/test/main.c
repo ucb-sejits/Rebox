d32e41c94d0cdc768f352b51a324ca0bcf9ea6dc
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-20 01:45:06 -0700

    accidentally made timing into float

--- c/test/main.c
+++ c/test/main.c
@@ -5,12 +5,12 @@ int main (int argc, const char* argv[])
 {
 	float* in = (float*) malloc(sizeof(float) * 1024*1024*1024);
 	float* out = (float*) malloc(sizeof(float) * 1024 * 1024 * 1024);
-	float t = omp_get_wtime();
+	double t = omp_get_wtime();
 	for (int i = 0; i < 5; i++)
 	{
 		apply(in, out);
 	}
-	float t2 = omp_get_wtime();
+	double t2 = omp_get_wtime();
 	free(in);
 	free(out);
 	printf("Time: %f\n", t2-t);
