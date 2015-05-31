2b0936f8232c310581d58677d988727bd8a68c50
Nathan Zhang <nzhang32@berkeley.edu>
2015-05-31 12:39:35 -0700

    fix to timing

--- c/indexing/decode_timing.c
+++ c/indexing/decode_timing.c
@@ -15,18 +15,28 @@ int main() {
 	size_t result;
 	size_t a, b, c;
 	for (uint64_t x = 0; x < SIZE; x++) {
-			for (uint64_t y = 0; y < SIZE; y++) {
-				for (uint64_t z = 0; z < SIZE; z++) {
-					result = encode(x, y, z);
-					decode(result, &a, &b, &c);
-					if (a != x || b != y || c != z) {
-						printf("%d\t\t%d-%d\t%d-%d\t%d-%d\n", result, a, x, b, y, c, z);
-					}
+		for (uint64_t y = 0; y < SIZE; y++) {
+			for (uint64_t z = 0; z < SIZE; z++) {
+				result = encode(x, y, z);
+				decode(result, &a, &b, &c);
+				if (a != x || b != y || c != z) {
+					printf("%d\t\t%d-%d\t%d-%d\t%d-%d\n", result, a, x, b, y, c, z);
 				}
 			}
 		}
+	}
 	clock_t end = clock();
+	for (uint64_t x = 0; x < SIZE; x++) {
+		for (uint64_t y = 0; y < SIZE; y++) {
+			for (uint64_t z = 0; z < SIZE; z++) {
+				result = encode(x, y, z);
+			}
+		}
+	}
+
+	clock_t encode_time = clock() - end;
+	clock_t diff = end - start;
 	//printf("%d\r", result>>63);
-	float elapsed = ((float) (end - start)) / CLOCKS_PER_SEC * 1000;
+	float elapsed = ((float) (diff - encode_time)) / CLOCKS_PER_SEC * 1000;
 	printf("Elapsed Time: %f ms\n", elapsed);
 }
