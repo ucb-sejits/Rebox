0b5b246e038ef255e7b97d2e9ea37885f92a81c9
Nathan Zhang <nzhang32@berkeley.edu>
2015-05-31 12:29:30 -0700

    fixed build_z

--- c/indexing/zdecode.c
+++ c/indexing/zdecode.c
@@ -4,28 +4,28 @@
 inline void decode(const size_t code, size_t* dim_0, size_t* dim_1, size_t* dim_2) {
     
     size_t tmp_0;
-    tmp_0 = code >> 0 & 0x249249u;
-    tmp_0 = (tmp_0 ^ tmp_0 >> 2) & 0xc30c3lu;
-    tmp_0 = (tmp_0 ^ tmp_0 >> 4) & 0xf00flu;
-    tmp_0 = (tmp_0 ^ tmp_0 >> 8) & 0xfflu;
+    tmp_0 = code >> 0 & 0x9249249u;
+    tmp_0 = (tmp_0 ^ tmp_0 >> 2) & 0xc30c30c30c3lu;
+    tmp_0 = (tmp_0 ^ tmp_0 >> 4) & 0xf00f00f00flu;
+    tmp_0 = (tmp_0 ^ tmp_0 >> 8) & 0xff0000fflu;
     tmp_0 = (tmp_0 ^ tmp_0 >> 16) & 0xfffflu;
     * dim_0 = tmp_0;
 
     
     size_t tmp_1;
-    tmp_1 = code >> 1 & 0x249249u;
-    tmp_1 = (tmp_1 ^ tmp_1 >> 2) & 0xc30c3lu;
-    tmp_1 = (tmp_1 ^ tmp_1 >> 4) & 0xf00flu;
-    tmp_1 = (tmp_1 ^ tmp_1 >> 8) & 0xfflu;
+    tmp_1 = code >> 1 & 0x9249249u;
+    tmp_1 = (tmp_1 ^ tmp_1 >> 2) & 0xc30c30c30c3lu;
+    tmp_1 = (tmp_1 ^ tmp_1 >> 4) & 0xf00f00f00flu;
+    tmp_1 = (tmp_1 ^ tmp_1 >> 8) & 0xff0000fflu;
     tmp_1 = (tmp_1 ^ tmp_1 >> 16) & 0xfffflu;
     * dim_1 = tmp_1;
 
     
     size_t tmp_2;
-    tmp_2 = code >> 2 & 0x249249u;
-    tmp_2 = (tmp_2 ^ tmp_2 >> 2) & 0xc30c3lu;
-    tmp_2 = (tmp_2 ^ tmp_2 >> 4) & 0xf00flu;
-    tmp_2 = (tmp_2 ^ tmp_2 >> 8) & 0xfflu;
+    tmp_2 = code >> 2 & 0x9249249u;
+    tmp_2 = (tmp_2 ^ tmp_2 >> 2) & 0xc30c30c30c3lu;
+    tmp_2 = (tmp_2 ^ tmp_2 >> 4) & 0xf00f00f00flu;
+    tmp_2 = (tmp_2 ^ tmp_2 >> 8) & 0xff0000fflu;
     tmp_2 = (tmp_2 ^ tmp_2 >> 16) & 0xfffflu;
     * dim_2 = tmp_2;
 
--- test/build_z.sh
+++ test/build_z.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python ../specializers/z/decode.py $1 > ../c/indexing/zdecode.c
 python ../specializers/z/encode.py $1 > ../c/indexing/zindex.c
-gcc-4.9 -std=c11 -O2 -DSIZE=$((2**$1)) ../c/indexing/decode_timing.c -o ../c/indexing/decode_timing
+gcc -std=c11 -O2 -w -DSIZE=$((2**$1)) ../c/indexing/decode_timing.c -o ../c/indexing/decode_timing
 echo $(../c/indexing/decode_timing)
